fe8afb053954d80642a327116c84725d7b141108
Anene Praise <anenepraise1@gmail.com>
2026-04-01 17:01:19 +0100

    feat: replace former icons with customizable svg icons

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import sunIcon from "@/assets/sun-icon.svg";
-import moonIcon from "@/assets/moon-icon.svg";
-import accessibilityIcon from "@/assets/accessibility-icon.svg";
-import Image from "next/image";
+import SunIcon from "@/assets/sun-icon.svg";
+import MoonIcon from "@/assets/moon-icon.svg";
+import AccessibilityIcon from "@/assets/accessibility-icon.svg";
 import clsx from "clsx";
 
 import { Rubik } from "next/font/google";
@@ -19,11 +18,14 @@ export default function Navbar() {
   return (
     <div className="navbar pt-4">
       <div className="navbar-start gap-6">
-        <Image
-          className={clsx(currentPath === "/" && "hidden")}
-          src={accessibilityIcon}
-          alt="Accessibility Icon"
-        />
+        <div>
+          <AccessibilityIcon
+            className={clsx(
+              "w-12 h-12 text-(--purple-600) bg-(--purple-100) rounded-sm",
+              currentPath === "/" && "hidden",
+            )}
+          />
+        </div>
         <p
           className={clsx(
             `${rubik.className} text-(--blue-900) text-2xl`,
@@ -34,12 +36,12 @@ export default function Navbar() {
         </p>
       </div>
       <div className="navbar-end">
-        <Image src={sunIcon} alt="Light mode" />
+        <SunIcon className="w-4 h-4 text-(--grey-500)" />
         <input
           className="toggle toggle-md bg-(--purple-600) text-white px-1 py-3 mx-2"
           type="checkbox"
         />
-        <Image src={moonIcon} alt="Dark mode" />
+        <MoonIcon className="w-4 h-4 text-(--grey-500)" />
       </div>
     </div>
   );
