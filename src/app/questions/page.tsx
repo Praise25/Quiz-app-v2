77cc8422871a4d0c2d7cf505b6045e8ba527e3c3
Anene Praise <anenepraise1@gmail.com>
2026-04-08 17:30:37 +0100

    feat: implement responsiveness from mobile up to 1024px screens

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -44,50 +44,56 @@ export default function Questions() {
   const { activeSubject } = useContext(AppContext);
 
   return (
-    <main className="mt-8 relative z-100 lg:flex lg:mt-12 ">
-      <p
-        className={`${rubikItalic.className} text-sm/[150%] text-(--grey-500)`}
-      >
-        Question 6 of 10
-      </p>
-      <h1
-        className={`${rubikMedium.className} text-[1.25rem]/[120%] mt-2 mb-6 lg:text-[2rem]`}
-      >
-        Which of these color contrast ratios defines the minimum WCAG 2.1 Level
-        AA requirement for normal text?
-      </h1>
+    <main className="mt-8 relative z-100 lg:flex lg:mt-12 gap-16">
+      <div className="lg:flex lg:flex-col lg:flex-1 lg:justify-between">
+        <div>
+          <p
+            className={`${rubikItalic.className} text-sm/[150%] text-(--grey-500)`}
+          >
+            Question 6 of 10
+          </p>
+          <h1
+            className={`${rubikMedium.className} text-[1.25rem]/[120%] mt-2 mb-6 lg:text-[2.25rem] xl:text-[4rem]`}
+          >
+            Which of these color contrast ratios defines the minimum WCAG 2.1
+            Level AA requirement for normal text?
+          </h1>
+        </div>
 
-      <div className="flex border-6 border-white rounded-full">
-        <progress
-          className={`progress progress-primary w-full ${activeSubject?.iconColor} bg-(--white)`}
-          value="40"
-          max="100"
-        ></progress>
+        <div className="flex border-6 border-white rounded-full lg:mb-20 lg:border-2">
+          <progress
+            className={`progress progress-primary w-full ${activeSubject?.iconColor} bg-(--white)`}
+            value="40"
+            max="100"
+          ></progress>
+        </div>
       </div>
 
-      <ul className="mt-10 flex flex-col gap-4 lg:flex-1 lg:mt-0 ">
-        {activeQuestion.options.map((option, index) => (
-          <li
-            key={option.id}
-            className="rounded-xl bg-(--white) border border-(--grey-50)"
-          >
-            <LinkButton className={`${rubikMedium.className} text-lg`}>
-              <div
-                className={`flex justify-center items-center w-10 h-10 mr-4 rounded-md p-2 bg-(--grey-50) text-(--grey-500)`}
-              >
-                {optionLetters[index]}
-              </div>
-              {option.label}
-            </LinkButton>
-          </li>
-        ))}
-      </ul>
+      <div className="lg:flex lg:flex-col lg:flex-1">
+        <ul className="mt-10 flex flex-col gap-4 lg:flex-1 lg:mt-0 ">
+          {activeQuestion.options.map((option, index) => (
+            <li
+              key={option.id}
+              className="rounded-xl bg-(--white) border border-(--grey-50)"
+            >
+              <LinkButton className={`${rubikMedium.className} text-lg`}>
+                <div
+                  className={`flex justify-center items-center w-10 h-10 mr-4 rounded-md p-2 bg-(--grey-50) text-(--grey-500)`}
+                >
+                  {optionLetters[index]}
+                </div>
+                {option.label}
+              </LinkButton>
+            </li>
+          ))}
+        </ul>
 
-      <button
-        className={`${rubikMedium.className} text-lg/[100%] text-(--white) flex justify-center items-center w-full h-14 p-4 mt-4 mb-12 rounded-xl ${activeSubject?.buttonBackgroundColor || "bg-(--purple-600)"} shadow-[0 16px 40px rgb(143 160 193 / 14%)]`}
-      >
-        Submit Answer
-      </button>
+        <button
+          className={`${rubikMedium.className} text-lg/[100%] text-(--white) flex justify-center items-center w-full h-14 p-4 mt-4 mb-12 rounded-xl ${activeSubject?.buttonBackgroundColor || "bg-(--purple-600)"} shadow-[0 16px 40px rgb(143 160 193 / 14%)]`}
+        >
+          Submit Answer
+        </button>
+      </div>
     </main>
   );
 }
